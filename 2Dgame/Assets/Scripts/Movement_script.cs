b67186f45463a6a762ba3d1b059a1792cf140ec6
Ryan Sandison <Ryan Sandison>
2016-10-12 16:42:26 +0100

    Very basic test of momement system

---
+++ 2Dgame/Assets/Scripts/Movement_script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+     
+    }
+
+   public int speed;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            this.transform.position += Vector3.left * speed * Time.deltaTime; 
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            this.transform.position += Vector3.right * speed * Time.deltaTime; 
+        }
+    }
+}
